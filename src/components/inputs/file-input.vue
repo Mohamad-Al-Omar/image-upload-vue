0dbc6e4d80b65933a062d07d130652b17e3fd924
Mohamad Al Omar <m.al.omar94@gmail.com>
2023-01-31 00:13:45 +0400

    create file input component

---
+++ src/components/inputs/file-input.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <v-btn
+      color="primary"
+      class="text-none"
+      rounded
+      depressed
+      :loading="isSelecting"
+      @click="onButtonClick"
+    >
+      Select Image
+    </v-btn>
+    <input
+      ref="uploader"
+      class="d-none"
+      type="file"
+      accept="image/*"
+      @change="onFileChanged"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FileInput",
+
+  data() {
+    return { selectedFile: null, isSelecting: false };
+  },
+
+  mounted() {},
+
+  methods: {
+    onButtonClick() {
+      this.isSelecting = true;
+      window.addEventListener(
+        "focus",
+        () => {
+          this.isSelecting = false;
+        },
+        { once: true }
+      );
+
+      this.$refs.uploader.click();
+    },
+    onFileChanged(e) {
+      this.selectedFile = e.target.files[0];
+
+      // do something
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
